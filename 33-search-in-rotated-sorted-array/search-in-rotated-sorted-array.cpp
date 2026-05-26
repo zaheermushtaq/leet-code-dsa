8f334de2a3d446c1d80a03e053cef68e569b6b04
zaheermushtaq <zaheer07zmnz@gmail.com>
2026-05-26 16:20:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -7,7 +7,6 @@ public:
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid]==target) return mid;
-
             if(nums[low]<=nums[mid]){
                 if(nums[low]<=target && target<nums[mid]){
                     high = mid-1;
