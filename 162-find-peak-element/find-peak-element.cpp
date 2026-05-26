d67e2bd77cac9453c6bd1cfbe78641ca40191573
zaheermushtaq <zaheer07zmnz@gmail.com>
2026-05-26 20:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (5.46%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if(n==1) return 0;
         if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-1]) return n-1;
+        if(nums[n-1]>nums[n-2]) return n-1;
         int low = 1;
         int high = n-2;
         while(low<=high){
